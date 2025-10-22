6adebdf1f53099ec24c0e0b3cad815c4c068e238
bizzleman <133555634+bizzleman@users.noreply.github.com>
2025-10-22 12:32:06 -0400

    Add validation program for grade input
    Created Unit3Assignment2.cpp to read a grade value and check if it lies within 0–100 using an if statement. Prints “The number is valid” when condition is true. Includes user input and basic validation logic.

---
+++ Unit3Assignment2.cpp
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Write an if statement that prints the message “The number is valid” if the
+variable grade is within the range 0 through 100.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int grade;
+
+  cout << "Write a variable grade: ";
+  cin >> grade;
+
+  if (grade >= 0 && grade <= 100) {
+    cout << "The number is valid" << endl;
+  }
+
+  return 0;
+}
