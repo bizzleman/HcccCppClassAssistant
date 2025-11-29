31badc401c7efcaa9ebf204c72adde1a7cf25903
bizzleman <133555634+bizzleman@users.noreply.github.com>
2025-11-28 21:59:51 -0500

    Add program to read and display numbers from file
    Added Unit5Assignment2.cpp, which reads integers from 'Numbers.txt' and prints them to the console. Included compiled executable Unit5Assignment2.exe.

---
+++ Unit5/Unit5Assignment2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    ifstream inputFile;
+    int number;
+
+    inputFile.open("Numbers.txt");
+
+    while (inputFile >> number) {
+        cout << number << endl;
+    }
+
+    inputFile.close();
+
+    return 0;
+}
