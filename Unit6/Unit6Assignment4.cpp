059c753b5437663fac20ee0602d42776d73cb1df
bizzleman <133555634+bizzleman@users.noreply.github.com>
2025-12-04 21:38:58 -0500

    Add corrected getValue function implementation
    Added Unit6Assignment4.cpp with a corrected version of the getValue function, fixing syntax errors related to reference parameters and usage.

---
+++ Unit6/Unit6Assignment4.cpp
@@ -0,0 +1,19 @@
+/*
+Locate as many errors as you can
+
+  void getValue(int value&) 
+
+{ 
+
+   cout << "Enter a value: "; 
+
+   cin >> value&; 
+
+} 
+*/
+
+void getValue(int& value) 
+{ 
+    cout << "Enter a value: "; 
+    cin >> value; 
+}
