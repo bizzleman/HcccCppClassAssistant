443fe0bfda858da930de6a6fbaef6297790ac065
bizzleman <133555634+bizzleman@users.noreply.github.com>
2025-12-04 21:32:30 -0500

    Add corrected total function implementation
    Added Unit6Assignment1.cpp with a corrected version of the total function, fixing parameter type declarations and function signature errors from the commented code.

---
+++ Unit6/Unit6Assignment1.cpp
@@ -0,0 +1,17 @@
+/*
+Locate as many error as you can
+
+
+ void total(int value1, value2, value3) 
+
+{ 
+
+   return value1 + value2 + value3; 
+
+} 
+*/
+
+int total(int value1, int value2, int value3)
+{
+    return value1 + value2 + value3;
+}
