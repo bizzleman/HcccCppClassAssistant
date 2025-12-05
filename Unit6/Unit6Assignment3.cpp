b9d0582b8b0c3a12652b86143939a24de7a3cf02
bizzleman <133555634+bizzleman@users.noreply.github.com>
2025-12-04 21:37:26 -0500

    Fix function parameter order and return type in area
    Corrected the area function by placing the default parameter last and specifying the correct return type. This resolves issues with default arguments and ensures the function returns the computed area.

---
+++ Unit6/Unit6Assignment3.cpp
@@ -0,0 +1,16 @@
+/*
+Locate as many errors as you can
+
+ void area(int length = 30, int width) 
+
+{ 
+
+   return length * width; 
+
+} 
+*/
+
+int area(int width, int length = 30)
+{ 
+    return length * width; 
+}
