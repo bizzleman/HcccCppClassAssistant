c586552041bfdf2b2f8836281b7fb7ab4da18eb1
bizzleman <133555634+bizzleman@users.noreply.github.com>
2025-12-04 21:35:17 -0500

    Add average calculation function
    Introduces a function to calculate the average of three integer values, correcting operator precedence and ensuring floating-point division. The function now returns the computed average.

---
+++ Unit6/Unit6Assignment2.cpp
@@ -0,0 +1,23 @@
+/*
+Locate as many errors as you can
+
+
+  double average(int value1, int value2, int value3) 
+
+{ 
+
+   double average; 
+
+   average = value1 + value2 + value3 / 3; 
+
+}
+*/
+
+double average(int value1, int value2, int value3) 
+{ 
+    double average; 
+
+    average = (value1 + value2 + value3) / 3.0; 
+
+    return average;
+}
